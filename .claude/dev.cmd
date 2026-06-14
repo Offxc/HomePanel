6dfd3e3aeedf54ff48ab127599f20b590564cd51
Off <mcvoidsmp@gmail.com>
2026-06-14 10:24:50 -0400

    Ignore .claude directory

--- .claude/dev.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "C:\Users\Admin\Documents\GitHub\LifePanel"
-set "PATH=C:\Users\Admin\AppData\Local\Microsoft\WinGet\Packages\OpenJS.NodeJS.LTS_Microsoft.Winget.Source_8wekyb3d8bbwe\node-v24.16.0-win-x64;%PATH%"
-node node_modules\next\dist\bin\next dev --port 3000
